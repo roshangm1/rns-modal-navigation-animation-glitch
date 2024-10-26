dbfcf609ce4406687334b87025fc9839642fdf9b
Brent Vatne <brentvatne@gmail.com>
2024-10-25 17:54:47 -0700

    Repro

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const RootStack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+
+function InnerScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="A" component={ScreenA} options={{ title: 'Screen A' }} />
+      <Stack.Screen name="B" component={ScreenB} options={{ title: 'Screen B' }} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <RootStack.Navigator>
+      <Stack.Screen name="Inner" component={InnerScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="C" component={ScreenC}
+        options={({ navigation, route }) => ({
+          presentation: 'modal',
+          headerLeft: () => <Button title="X" onPress={() => navigation.goBack()} />,
+        })}
+      />
+    </RootStack.Navigator>
+  );
+}
+
+/* Boring placeholder screens */
+
+function ScreenA({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'red' }}>A</Text></Text>
+      <Button title="Go to B" onPress={() => navigation.navigate('B')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ScreenB({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'green' }}>B</Text></Text>
+      <Button title="Go to C" onPress={() => navigation.navigate('C')} />
+    </View>
+  );
+}
+
+function ScreenC() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Screen C (Modal)</Text>
+    </View>
+  );
+}
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+export default function AppContainer() {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
+
+registerRootComponent(AppContainer);
